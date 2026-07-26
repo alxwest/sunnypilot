b23b8a8b4212c191611bb6e06e18e21efae638f4
Alex West <alxwest@gmail.com>
2026-07-26 18:35:09 +0100

    Restart sensord when IMU interrupts stall

--- openpilot/system/manager/process_config.py
+++ openpilot/system/manager/process_config.py
@@ -137,7 +137,8 @@ procs = [
   PythonProcess("modeld", "openpilot.selfdrive.modeld.modeld", and_(only_onroad, is_stock_model)),
   PythonProcess("dmonitoringmodeld", "openpilot.selfdrive.modeld.dmonitoringmodeld", driverview, enabled=(WEBCAM or not PC)),
 
-  PythonProcess("sensord", "openpilot.system.sensord.sensord", or_(only_onroad, parking_recording), enabled=not PC),
+  PythonProcess("sensord", "openpilot.system.sensord.sensord", or_(only_onroad, parking_recording),
+                enabled=not PC, restart_if_crash=True),
   PythonProcess("ui", "openpilot.selfdrive.ui.ui", always_run, restart_if_crash=True),
   PythonProcess("soundd", "openpilot.selfdrive.ui.soundd", driverview),
   PythonProcess("locationd", "openpilot.selfdrive.locationd.locationd", only_onroad),
